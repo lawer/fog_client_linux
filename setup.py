abd3c0307e22a2c369f249ed24102a9586418827
Carles Gonzalez <carlesgm@gmail.com>
2012-08-31 00:49:01 +0200

    Version 0.3.2, setuptools

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
-from distutils.core import setup
+#from distutils.core import setup
 
-setup(name='fog_client',
-      version='0.3.1',
-      author='Carles Gonzalez',
-      packages=['components', 'cliapp'],
-      py_modules=['fog_lib', 'fog_client', 'scheduler'],
-      requires=['cuisine',
-                'requests (>=0.13)'],
-      scripts=['fog_client.py'],
-      data_files=[('/etc/init', ['aux/fog_client.conf']),
-                  ('/etc', ['aux/fog_client.ini'])],
-      classifiers=[
-             'Development Status :: 3 - Alpha',
-             'Environment :: Console',
-             'Intended Audience :: System Administrators',
-             'Operating System :: POSIX',
-             'Programming Language :: Python',
-             'Topic :: System :: Systems Administration'])
+from setuptools import setup, find_packages
+
+setup(
+    name='fog_client',
+    version='0.3.2',
+    author='Carles Gonzalez',
+    packages=find_packages(),
+    py_modules=['fog_lib', 'fog_client'],
+    install_requires=['cuisine', 'requests>=0.13'],
+    scripts=['fog_client.py'],
+    data_files=[('/etc/init', ['aux/fog_client.conf']),
+                ('/etc', ['aux/fog_client.ini'])],
+    classifiers=[
+         'Development Status :: 3 - Alpha',
+         'Environment :: Console',
+         'Intended Audience :: System Administrators',
+         'Operating System :: POSIX',
+         'Programming Language :: Python',
+         'Topic :: System :: Systems Administration'])
