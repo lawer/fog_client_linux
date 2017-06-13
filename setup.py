0badf6e8173dbcbf5bf84987a6db291ea5bce5c0
Carles <lawer@users.noreply.github.com>
2017-06-13 09:45:19 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='fog_client',
-    version='0.6.4',
+    version='0.6.5',
     author='Carles Gonzalez',
     packages=['components', 'cliapp', ],
     py_modules=['fog_lib', 'fog_client'],
